ca069ae22aa6c63b1f9d35e843160f87e5790151
risheeboy <rishee.boy@gmail.com>
2025-11-09 17:14:26 +0800

    refact: changed server port to 8080

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ if __name__ == '__main__':
     os.makedirs('static', exist_ok=True)
 
     # Run the application
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
